bb8b001c991d53f44e922be5575f6329b4e9e348
twilight <twi@gmail.com>
2024-07-07 00:04:20 +0530

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1); // easy
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); 
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// avoid these types of comparison as listed above
+// compare only similar datatypes
+
+
+// ===
+console.log("2" === 2);
+
